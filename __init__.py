76e6521a35d0fc22a3efef56202371297bf22be4
Chris Billington <chrisjbillington@gmail.com>
2014-06-02 16:04:42 +1000

    Added skeleton of specification

---
+++ __init__.py
@@ -0,0 +1,27 @@
+__version__ = '0.1.0-dev'
+
+def LabscriptDevice(class_):
+    raise NotImplementedError
+
+def BLACSTab(class_):
+    raise NotImplementedError
+
+def BLACSWorker(class_):
+    raise NotImplementedError
+    
+def RunviewerParser(class_):
+    raise NotImplementedError
+    
+def get_labscript_device(name):
+    raise NotImplementedError
+    
+def get_BLACS_tab(name):
+    raise NotImplementedError
+
+def get_BLACS_worker(name):
+    raise NotImplementedError
+        
+def get_runviewer_parser(name):
+    raise NotImplementedError
+    
+
