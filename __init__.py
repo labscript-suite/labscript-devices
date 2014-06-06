c236de57c02f8497baa3ec0999aa8f20d6143078
Chris Billington <chrisjbillington@gmail.com>
2014-06-06 12:10:29 +1000

    Finished mechanism for defining and retrieving the different tpyes of classes

--- __init__.py
+++ __init__.py
@@ -1,8 +1,10 @@
+import os
 import sys
+import importlib
 
 __version__ = '0.1.0-dev'
 
-def ClassRegister(object):
+class ClassRegister(object):
     """A register for looking up classes by module name.  Provides a
      decorator and a method for looking up classes decorated with it,
      importing as necessary."""
@@ -16,54 +18,68 @@ def ClassRegister(object):
         by module name"""
         # Add an attribute to the class so it knows its own name in case
         # it needs to look up other classes in the same module:
-        cls.labscript_device_name = cls_.__module__.split('.')[-1]
+        cls.labscript_device_class_name = cls.__module__.split('.')[-1]
+        if cls.labscript_device_class_name == '__main__':
+            # User is running the module as __main__. Use the filename instead:
+            import __main__
+            try:
+                cls.labscript_device_class_name = os.path.splitext(os.path.basename(__main__.__file__))[0]
+            except AttributeError:
+                # Maybe they're running interactively? Or some other funky environment. Either way, we can't proceed.
+                raise RuntimeError('Can\'t figure out what the file or module this class is being defined in. ' +
+                                   'If you are testing, please test from a more standard environment, such as ' +
+                                   'executing a script from the command line, or if you are using an interactive session, ' +
+                                   'writing your code in a separate module and importing it.')
+                
         # Add it to the register:
-        self.registered_classes[cls.labscript_device_name] = cls
+        self.registered_classes[cls.labscript_device_class_name] = cls
         return cls
         
     def __getitem__(self, name):
         try:
+            # Ensure the module's code has run (this does not re-import it if it is already in sys.modules)
+            importlib.import_module('.' + name, __name__)
+        except ImportError:
+            sys.stderr.write('Error importing module %s.%s whilst looking for classes for device %s. '%(__name__, name, name) +
+                             'Check that the module exists, is named correctly, and can be imported with no errors. ' +
+                             'Full traceback follows:\n')
+        # Class definitions in that module have executed now, check to see if class is in our register:
+        try:
             return self.registered_classes[name]
         except KeyError:
-            pass
-        # If we haven't seen that class, let's see if we can import it:
-        full_module_name = __name__ + '.' + name
-        if full_module_name in sys.modules:
-            # Hm, already imported but not in our register. Maybe the user forgot to decorate it?
-            raise ValueError('No class found in module %s, did you forget to decorate the class definition with @%s?'%(self.description))
+            # No? No such class is defined then, or maybe the user forgot to decorate it.
+            raise ValueError('No class decorated as a %s as found in module %s, '%(self.instancename, __name__ + '.' + name) + 
+                             'Did you forget to decorate the class definition with @%s?'%(self.instancename))
 
+class SameNameClassRegister(ClassRegister):
+    """Subclass of ClassRegister that also checks that the
+    class has the same name as the file it is in."""
+    def __call__(self, cls):
+        ClassRegister.__call__(self, cls)
+        if cls.labscript_device_class_name != cls.__name__:
+            raise ValueError('The class decorated as a @labscript_device must have the same name as the file it is in. ' +
+                             'For example NI_PCI_6733.py: class NI_PCI_6733(IntermediateDevice). ' +
+                             'Otherwise labscript suite programs looking for it won\'t know what file to look in!')
+        return cls
+        
+# The decorators the user should apply to their classes so that the
+# respective programs can look them up:
+labscript_device = SameNameClassRegister('labscript_device')
+BLACS_tab = ClassRegister('BLACS_tab')
+BLACS_worker = ClassRegister('BLACS_worker')
+runviewer_parser = ClassRegister('runviewer_parser')
 
-############################################
-# Temporary compat for current runviewer development until above code
-# is more general
-
-import importlib
-
-__runviewer_classes__ = {}
-
-def import_device(device):
-    module = importlib.import_module('.%s'%device, 'labscript_devices')
-
-def RunviewerParser(the_class):
-    class_name = the_class.__module__.split('.')[-1]
-    __runviewer_classes__[class_name] = the_class
-    return the_class
-    
-def get_runviewer_class(device_class_name):
-    return __runviewer_classes__[device_class_name]
-    
-#############################################
-
+# Wrapper functions to get devices out of the class registers.
 def get_labscript_device(name):
-    raise NotImplementedError
+    return labscript_device[name]
     
 def get_BLACS_tab(name):
-    raise NotImplementedError
+    return BLACS_tab[name]
 
 def get_BLACS_worker(name):
-    raise NotImplementedError
+    return BLACS_worker[name]
         
 def get_runviewer_parser(name):
-    raise NotImplementedError
+    return runviewer_parser[name]
     
 
--- test_device.py
+++ test_device.py
@@ -1,8 +1,18 @@
 import sys
-sys.path.append('..')
+from labscript_devices import labscript_device, BLACS_tab, BLACS_worker, runviewer_parser 
 
-from labscript_devices import BLACSTab, LabscriptDevice
-
-@LabscriptDevice
+@labscript_device
 class test_device(object):
+    pass
+
+@BLACS_tab
+class Tab(object):
+    pass
+    
+@BLACS_worker
+class Worker(object):
+    pass
+    
+@runviewer_parser
+class Parser(object):
     pass
