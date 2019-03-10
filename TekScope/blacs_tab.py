8093b2815a8e2b37c5cd279efa130c9e3583e92c
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2019-03-10 16:18:06 +1100

    TekScope: Made initialise_GUI of blacs_tab empty in honour of it being a GUIless tab.

--- TekScope/blacs_tab.py
+++ TekScope/blacs_tab.py
@@ -6,14 +6,11 @@ from blacs.device_base_class import DeviceTab
 
 class TekScopeTab(DeviceTab):
     def initialise_GUI(self):
-        self.address = self.BLACS_connection
-        connection_object = self.settings['connection_table'].find_by_name(self.device_name)
-        self.connection_table_properties = connection_object.properties
+        pass
 
     def initialise_workers(self):
-        worker_initialisation_kwargs = {'addr': self.address}
-        for kwarg in ['termination', 'preamble_string']:
-            worker_initialisation_kwargs[kwarg] = self.connection_table_properties[kwarg]
+        worker_initialisation_kwargs = self.connection_table.find_by_name(self.device_name).properties
+        worker_initialisation_kwargs['addr'] = self.BLACS_connection
         self.create_worker(
             'main_worker',
             'labscript_devices.TekScope.blacs_worker.TekScopeWorker',
