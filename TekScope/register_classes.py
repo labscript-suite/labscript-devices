5e4d8348b7955511b0b8fc6fb8bbd2b4d81bcbbf
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2019-03-10 16:26:49 +1100

    TekScope: Use plural submodule names, i.e. blacs_tabs, blacs_workers, labscript_devices. Device instantiation should now use from labscript_devices.TekScope.labscript_devices import TekScope

--- TekScope/register_classes.py
+++ TekScope/register_classes.py
@@ -2,6 +2,6 @@ import labscript_devices
 
 labscript_devices.register_classes(
     'TekScope',
-    BLACS_tab='labscript_devices.TekScope.blacs_tab.TekScopeTab',
+    BLACS_tab='labscript_devices.TekScope.blacs_tabs.TekScopeTab',
     runviewer_parser=None
 )
