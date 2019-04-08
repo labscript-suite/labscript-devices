8b43848f70399a793400a2cb01c2b8b1c539b461
Chris Billington <chrisjbillington@gmail.com>
2019-04-08 15:34:28 -0400

    IMAQdxCamera: Require Python 3.6, labscript 2.5.2

--- IMAQdxCamera/__init__.py
+++ IMAQdxCamera/__init__.py
@@ -1,9 +1,9 @@
 from labscript_utils import check_version
 
-from labscript_utils import PY2
-if PY2:
-    raise RuntimeError("IMAQdxCamera driver not compatible with Python 2")
+import sys
+if sys.version_info < (3, 6):
+    raise RuntimeError("IMAQdxCamera driver requires Python 3.6+")
 
 
 check_version('labscript_utils', '2.12.1', '3')
-check_version('labscript', '2.5.1', '3')
+check_version('labscript', '2.5.2', '3')
