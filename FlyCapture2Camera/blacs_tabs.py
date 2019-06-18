255253019341cbfcbd2dcedbcf792db3e1600f8d
Warm Vapor Control <dihm.meyer@gmail.com>
2019-06-18 10:13:49 -0400

    FlyCapture2Camera: Adds a sub-class of IMAQdxCamera for use with the FlyCapture2/PyCapture2 backend for Point Grey cameras.
    Camera attributes are set by a nested dictionary due to inherent craziness of
    the FlyCapture2 API. Due to this, camera settings can't be saved to the h5 file
    since nested dictionaries are not natively supported by h5py.

---
+++ FlyCapture2Camera/blacs_tabs.py
@@ -0,0 +1,20 @@
+#####################################################################
+#                                                                   #
+# /labscript_devices/FlyCapture2Camera/blacs_tabs.py                #
+#                                                                   #
+# Copyright 2019, Monash University and contributors                #
+#                                                                   #
+# This file is part of labscript_devices, in the labscript suite    #
+# (see http://labscriptsuite.org), and is licensed under the        #
+# Simplified BSD License. See the license.txt file in the root of   #
+# the project for the full license.                                 #
+#                                                                   #
+#####################################################################
+
+from labscript_devices.IMAQdxCamera.blacs_tabs import IMAQdxCameraTab
+
+class FlyCapture2CameraTab(IMAQdxCameraTab):
+    
+    # override worker class
+    worker_class = 'labscript_devices.FlyCapture2Camera.blacs_workers.FlyCapture2CameraWorker'
+
---
+++ FlyCapture2Camera/labscript_devices.py
@@ -0,0 +1,18 @@
+#####################################################################
+#                                                                   #
+# /labscript_devices/FlyCapture2Camera/labscript_devices.py         #
+#                                                                   #
+# Copyright 2019, Monash University and contributors                #
+#                                                                   #
+# This file is part of labscript_devices, in the labscript suite    #
+# (see http://labscriptsuite.org), and is licensed under the        #
+# Simplified BSD License. See the license.txt file in the root of   #
+# the project for the full license.                                 #
+#                                                                   #
+#####################################################################
+
+from labscript_devices.IMAQdxCamera.labscript_devices import IMAQdxCamera
+
+class FlyCapture2Camera(IMAQdxCamera):
+    description = 'FlyCapture2 Camera'
+
