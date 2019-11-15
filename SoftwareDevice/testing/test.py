72fb9edc1e40e29e6514d4cc803b58d61ea5a12c
Chris Billington <chrisjbillington@gmail.com>
2019-11-14 21:45:33 -0500

    SoftwareDevice: Missing copyright notices

--- SoftwareDevice/testing/test.py
+++ SoftwareDevice/testing/test.py
@@ -7,10 +7,12 @@ labscript_init('test.h5', new=True, overwrite=True)
 DummyPseudoclock('pseudoclock')
 SoftwareDevice('software_device')
 
+
 def foo(shot_context, t, arg):
     print(f"hello, {arg}!")
-start()
+
 
 software_device.add_function('start', foo, 'world')
 
-stop(2)
+start()
+stop(1)
--- SoftwareDevice/utils.py
+++ SoftwareDevice/utils.py
@@ -1,3 +1,16 @@
+#####################################################################
+#                                                                   #
+# /labscript_devices/SoftwareDevice/utils.py                        #
+#                                                                   #
+# Copyright 2019, Monash University and contributors                #
+#                                                                   #
+# This file is part of labscript_devices, in the labscript suite    #
+# (see http://labscriptsuite.org), and is licensed under the        #
+# Simplified BSD License. See the license.txt file in the root of   #
+# the project for the full license.                                 #
+#                                                                   #
+#####################################################################
+
 import inspect
 from types import FunctionType
 from labscript_utils import dedent
