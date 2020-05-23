80ed3b625c0e816036e919fcf6f8b759c7dd29c2
chrisjbillington <chrisjbillington@gmail.com>
2020-05-23 16:31:14 -0400

    Remove PY2 compat from DAQmx template and regenerate all class files

--- labscript_devices/NI_DAQmx/models/generate_subclasses.py
+++ labscript_devices/NI_DAQmx/models/generate_subclasses.py
@@ -10,14 +10,6 @@
 # file in the root of the project for the full license.             #
 #                                                                   #
 #####################################################################
-from __future__ import division, unicode_literals, print_function, absolute_import
-from labscript_utils import PY2
-
-if PY2:
-    str = unicode
-    import io
-    open = io.open
-
 import os
 import warnings
 import json
