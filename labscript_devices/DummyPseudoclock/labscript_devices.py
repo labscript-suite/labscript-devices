d2e5679174498e4cb5f6049c32962bc97864461f
Russell Anderson <5637107+rpanderson@users.noreply.github.com>
2020-06-12 18:47:25 +1000

    Save clock instructions (mock PineBlaster) and add runviewer_parser

--- labscript_devices/DummyPseudoclock/labscript_devices.py
+++ labscript_devices/DummyPseudoclock/labscript_devices.py
@@ -15,24 +15,90 @@
 # and labscript. The device is a PseudoclockDevice, and can be the sole device
 # in a connection table or experiment.
 
-import labscript_utils.h5_lock
-import h5py
-from labscript import PseudoclockDevice, Pseudoclock, ClockLine
+from labscript import PseudoclockDevice, Pseudoclock, ClockLine, config, LabscriptError
+import numpy as np
 
 
 class DummyPseudoclock(PseudoclockDevice):
 
     description = 'Dummy pseudoclock'
-    clock_limit = 1e6
-    clock_resolution = 1e-6
+    clock_limit = 10e6
+    clock_resolution = 25e-9
+    clock_type = 'fast clock'
+    trigger_delay = 350e-9
+    wait_delay = 2.5e-6
+    allowed_children = None
+    max_instructions = 1e5
 
-    def __init__(self, name='dummy_pseudoclock', BLACS_connection='dummy_connection', **kwargs):
+    def __init__(
+        self, name='dummy_pseudoclock', BLACS_connection='dummy_connection', **kwargs
+    ):
         self.BLACS_connection = BLACS_connection
         PseudoclockDevice.__init__(self, name, None, None, **kwargs)
-        self.pseudoclock = Pseudoclock(self.name + '_pseudoclock', self, 'pseudoclock')
-        self.clockline = ClockLine(name='clockline', pseudoclock=self.pseudoclock, connection='dummy')
+        self._pseudoclock = Pseudoclock(
+            name=f'{name}_pseudoclock',
+            pseudoclock_device=self,
+            connection='pseudoclock',
+        )
+        self._clock_line = ClockLine(
+            name=f'{name}_clock_line',
+            pseudoclock=self.pseudoclock,
+            connection='internal',
+        )
+
+    @property
+    def pseudoclock(self):
+        return self._pseudoclock
+
+    @property
+    def clockline(self):
+        return self._clock_line
 
     def generate_code(self, hdf5_file):
         PseudoclockDevice.generate_code(self, hdf5_file)
         group = self.init_device_group(hdf5_file)
+
+        # Compress clock instructions with the same period
+        # This will halve the number of instructions roughly,
+        # since the DummyPseudoclock does not have a 'slow clock'
+        reduced_instructions = []
+        for instruction in self.pseudoclock.clock:
+            if instruction == 'WAIT':
+                # The following period and reps indicates a wait instruction
+                reduced_instructions.append({'period': 0, 'reps': 1})
+                continue
+            reps = instruction['reps']
+            # period is in quantised units:
+            period = int(round(instruction['step'] / self.clock_resolution))
+            if reduced_instructions and reduced_instructions[-1]['period'] == period:
+                reduced_instructions[-1]['reps'] += reps
+            else:
+                reduced_instructions.append({'period': period, 'reps': reps})
+        # The following period and reps indicates a stop instruction:
+        reduced_instructions.append({'period': 0, 'reps': 0})
+        if len(reduced_instructions) > self.max_instructions:
+            raise LabscriptError(
+                "%s %s has too many instructions. It has %d and can only support %d"
+                % (
+                    self.description,
+                    self.name,
+                    len(reduced_instructions),
+                    self.max_instructions,
+                )
+            )
+        # Store these instructions to the h5 file:
+        dtypes = [('period', int), ('reps', int)]
+        pulse_program = np.zeros(len(reduced_instructions), dtype=dtypes)
+        for i, instruction in enumerate(reduced_instructions):
+            pulse_program[i]['period'] = instruction['period']
+            pulse_program[i]['reps'] = instruction['reps']
+        group.create_dataset(
+            'PULSE_PROGRAM', compression=config.compression, data=pulse_program
+        )
+        # TODO: is this needed, the PulseBlasters don't save it...
+        self.set_property(
+            'is_master_pseudoclock',
+            self.is_master_pseudoclock,
+            location='device_properties',
+        )
         self.set_property('stop_time', self.stop_time, location='device_properties')
