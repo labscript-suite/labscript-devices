87468a1220a7724618e09be3a3d7d7850c2734a8
David Meyer <dihm.meyer@gmail.com>
2022-03-03 09:32:36 -0500

    Update to use `FreqConversion` class in mainline labscript_utils.
    Also cleans up a few linter warnings.

--- labscript_devices/Windfreak/labscript_devices.py
+++ labscript_devices/Windfreak/labscript_devices.py
@@ -13,61 +13,23 @@
 
 from labscript import LabscriptError, set_passed_properties, config, StaticDDS, IntermediateDevice, Device
 from labscript_utils import dedent
-from labscript_utils.unitconversions.UnitConversionBase import UnitConversion
-
-class FreqConversion(UnitConversion):
-    """
-    A Generic frequency conversion class that covers standard SI prefixes from a base of Hz.
-    """
-
-    base_unit = 'Hz' # must be defined here and match default hardware unit in BLACS tab
-
-    def __init__(self, calibration_parameters = None):
-        self.parameters = calibration_parameters
-        if hasattr(self, 'derived_units'):
-            self.derived_units += ['kHz', 'MHz', 'GHz']
-        else:
-            self.derived_units = ['kHz', 'MHz', 'GHz']
-        UnitConversion.__init__(self,self.parameters)
-    
-    def kHz_to_base(self,kHz):
-        Hz = kHz*1e3
-        return Hz
-
-    def kHz_from_base(self,Hz):
-        kHz = Hz*1e-3
-        return kHz
-
-    def MHz_to_base(self,MHz):
-        Hz = MHz*1e6
-        return Hz
-
-    def MHz_from_base(self,Hz):
-        MHz = Hz*1e-6
-        return MHz
-
-    def GHz_to_base(self,GHz):
-        Hz = GHz*1e9
-        return Hz
-
-    def GHz_from_base(self,Hz):
-        GHz = Hz*1e-9
-        return GHz    
+from labscript_utils.unitconversions.generic_frequency import FreqConversion
 
 import numpy as np
+
 
 class WindfreakSynth(Device):
     description = 'Windfreak HDPro Synthesizer'
     allowed_children = [StaticDDS]
     # note, box labels 'A', 'B' map to programming channels 0, 1
-    allowed_chans = [0,1]
+    allowed_chans = [0, 1]
 
     # define output limitations for the SynthHDPro
-    freq_limits = (10e6,24e9) # set in Hz
-    freq_res = 1 # number of sig digits after decimal
-    amp_limits = (-40.0,20.0) # set in dBm
+    freq_limits = (10e6, 24e9)  # set in Hz
+    freq_res = 1  # number of sig digits after decimal
+    amp_limits = (-40.0, 20.0)  # set in dBm
     amp_res = 2
-    phase_limits = (0.0,360.0) # in deg
+    phase_limits = (0.0, 360.0)  # in deg
     phase_res = 2
 
     @set_passed_properties(property_names={
@@ -85,7 +47,7 @@ class WindfreakSynth(Device):
     })
     def __init__(self, name, com_port="", trigger_mode='disabled', **kwargs):
         """Creates a Windfreak HDPro Synthesizer
-        
+
         Args:
             name (str): python variable name to assign the device to.
             com_port (str): COM port connection string.
@@ -110,7 +72,7 @@ class WindfreakSynth(Device):
 
     def get_default_unit_conversion_classes(self, device):
         """Child devices call this during their `__init__` to get default unit conversions.
-        
+
         If user has not overridden, will use generic FreqConversion class.        
         """
 
@@ -118,7 +80,7 @@ class WindfreakSynth(Device):
 
     def validate_data(self, data, limits, device):
         """Tests that requested data is within limits.
-        
+
         Args:
             data (iterable or numeric): Data to be checked.
                 Input is cast to a numpy array of type float64.
@@ -135,11 +97,10 @@ class WindfreakSynth(Device):
             {limits[0]:E}Hz and {limits[1]:E}Hz, {data} given
             '''
             raise LabscriptError(dedent(msg))
-        return data   
+        return data
 
     def generate_code(self, hdf5_file):
         DDSs = {}
-        
 
         for output in self.child_devices:
 
