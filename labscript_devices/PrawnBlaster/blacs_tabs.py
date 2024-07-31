e995ef9a404090113b77599936cbdc56b8a742de
Shafin <haqueshafinul@gmail.com>
2024-07-31 10:20:11 -0400

    schedule prawnblaster status monitor every 5ms instead of 100ms

--- labscript_devices/PrawnBlaster/blacs_tabs.py
+++ labscript_devices/PrawnBlaster/blacs_tabs.py
@@ -159,4 +159,4 @@ class PrawnBlasterTab(DeviceTab):
         self.statemachine_timeout_remove(self.status_monitor)
         yield (self.queue_work(self.primary_worker, "start_run"))
         self.status_monitor()
-        self.statemachine_timeout_add(100, self.status_monitor, notify_queue)
+        self.statemachine_timeout_add(5, self.status_monitor, notify_queue)
