8f007b53780835858dda36e733d850bb4d16bd4a
Shafin <haqueshafinul@gmail.com>
2024-08-06 11:20:01 -0400

    fix register_classes import path

--- labscript_devices/RemoteControl/register_classes.py
+++ labscript_devices/RemoteControl/register_classes.py
@@ -2,6 +2,6 @@ from labscript_devices import register_classes
 
 register_classes(
     'RemoteControl',
-    BLACS_tab='user_devices.RemoteControl.blacs_tabs.RemoteControlTab',
+    BLACS_tab='labscript_devices.RemoteControl.blacs_tabs.RemoteControlTab',
     runviewer_parser=None
 )
